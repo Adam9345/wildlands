0a7070b2d73dd499094464f74e1b566657b1df63
adamklo93 <klonica3a@gmail.com>
2018-12-29 20:58:10 +0100

    first commit

---
+++ desktop/src/com/wildlands/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wildlands.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wildlands.WildlandsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WildlandsGame(), config);
+	}
+}
